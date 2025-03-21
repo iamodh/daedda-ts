d6610c90d59750767bacf9f3f74805dd27f5f72b
Donghwan Oh <imodh97@gmail.com>
2025-03-21 21:55:15 +0900

    chore: import 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import router from "./routes/routes.tsx";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "styled-components";
-import theme from "styles/theme.ts";
+import theme from "@styles/theme.ts";
+import router from "@routes/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
