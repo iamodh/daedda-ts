3995ffbfb4567657f653a761b30cc5437bdecade
Donghwan Oh <imodh97@gmail.com>
2025-03-23 11:17:07 +0900

    feat: ToggleButton 컴포넌트 완성

--- src/components/common/ToggleButton.tsx
+++ src/components/common/ToggleButton.tsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 type ToggleButtonProps = {
   children: React.ReactNode;
 };
 
-const Container = styled.div`
-  border: ${(props) => props.theme.colors.gray[300]} solid 2px;
+const Container = styled.button<{ $isActive: boolean }>`
+  border: ${(props) => {
+      return props.$isActive
+        ? props.theme.colors.primary[400]
+        : props.theme.colors.gray[300];
+    }}
+    solid 2px;
   border-radius: ${(props) => props.theme.borderRadius.md};
+  transition: border 0.1s ease-in;
   padding: 6px 12px;
   font-size: ${(props) => props.theme.fontSizes.sm};
   cursor: pointer;
 `;
 
 const ToggleButton = ({ children }: ToggleButtonProps) => {
-  return <Container>{children}</Container>;
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  return (
+    <Container
+      $isActive={isActive}
+      onClick={handleClick}
+      aria-pressed={isActive}
+    >
+      {children}
+    </Container>
+  );
 };
 
 export default ToggleButton;
